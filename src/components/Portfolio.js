f9b3f0bed4911bd24106345757387a88971df69c
fearoffun <480dunce@gmail.com>
2023-09-11 23:50:34 -0700

    portfolio in the works....

---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <div className="main">
+                <h1>Niko Vu</h1>
+                <img src="https://nikovu.weebly.com/uploads/1/3/6/9/136968252/mi-stake_orig.gif"/>
+            </div>
+        </div>
+    );
+}
+
+export default Portfolio;
